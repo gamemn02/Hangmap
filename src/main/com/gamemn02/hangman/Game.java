0b2621b2f0adb34b3c75bfc01dd5f5e2183959a0
Nadhir Boukorsi <gamemn02@gmail.com>
2019-03-25 15:15:52 +0100

    define the game api methods

--- src/main/com/gamemn02/hangman/Game.java
+++ src/main/com/gamemn02/hangman/Game.java
@@ -1,4 +1,25 @@
 package com.gamemn02.hangman;
 
+
+import java.util.Iterator;
+import java.util.Observable;
+import java.util.Observer;
+
 public class Game {
+
+    public Game(int appendage) {
+    }
+
+    public boolean setPhrase(String phrase) {
+        return false;
+    }
+
+    public void play(char letter) {
+
+    }
+
+    public void observe(Observer winObserver, Observer lossObserver) {
+
+    }
+
 }
---
+++ src/test/com/gamemn02/hangman/GameTest.java
@@ -0,0 +1,10 @@
+package com.gamemn02.hangman;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    public void apiWorksSuccessful() {
+
+    }
+}
